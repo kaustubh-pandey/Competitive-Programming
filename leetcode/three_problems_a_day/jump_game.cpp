fbd76f1a5026528e4f90412322b9f05fa00fa8a1
kaustubh-pandey <pandeykaustubh6@gmail.com>
2020-03-10 00:44:57 +0530

    Added O(n) greedy solun

--- leetcode/three_problems_a_day/jump_game.cpp
+++ leetcode/three_problems_a_day/jump_game.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//O(n^2) bottom up DP
+/*class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
@@ -17,5 +18,22 @@ public:
             }
         }
         return dp[n-1];
+    }
+};*/
+//O(n) greedy
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0){
+            return true;
+        }
+        int curr_idx = n-1;
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]+i >= curr_idx){
+                curr_idx = i;
+            }
+        }
+        return curr_idx == 0;
     }
 };
