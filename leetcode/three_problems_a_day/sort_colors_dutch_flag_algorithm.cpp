18cc06a663fbf83d087e267e7f0d0661a71660fc
kaustubh-pandey <pandeykaustubh6@gmail.com>
2020-08-19 22:29:20 +0530

    Dutch NationalFlag Algorithm

---
+++ leetcode/three_problems_a_day/sort_colors_dutch_flag_algorithm.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //dutch national flag algorithm
+        int n = nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low++],nums[mid++]);
+            }
+            else if(nums[mid]==2){
+                swap(nums[high--],nums[mid]);
+            }
+            else{mid++;}
+        }
+    }
+};
